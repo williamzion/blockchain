c3538291537a986340e96542c309050ab6ee59ca
Willaim <1340440725fhs@gmail.com>
2019-03-05 22:54:32 +0800

    proof-of-work implementation

---
+++ proofofwork.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+// Global limiter for avoiding nonce increment overflow.
+var maxNonce = math.MaxInt64
+
+// Difficulty of mining.
+const targetBits = 24
+
+// ProofOfWork represents proof-of-work.
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+// NewProofOfWork builds and returns a ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+// prepareData merges block fields with the target and the nonce into a single
+// slice of bytes and returns it.
+// Here nonce is the counter from the Bitcoin Hashcash description.
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+// Run is the core of PoW algorithm, it performs the proof-of-work.
+func (pow *ProofOfWork) Run() (int, []byte) {
+	// Integer representation of hash.
+	var hashInt big.Int
+	var hash [32]byte
+	// The counter.
+	nonce := 0
+
+	fmt.Printf("Mining the block containing %q\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
