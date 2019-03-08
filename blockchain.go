88444089cb18da51182ff517f15c47693d4ec1b3
Willaim <1340440725fhs@gmail.com>
2019-03-08 21:29:57 +0800

    Don't close db connection, it should be reused.

--- blockchain.go
+++ blockchain.go
@@ -34,7 +34,6 @@ func NewBlockChain() *Blockchain {
 	if err != nil {
 		log.Panic(err)
 	}
-	defer db.Close()
 
 	err = db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
@@ -63,7 +62,7 @@ func NewBlockChain() *Blockchain {
 		} else {
 			tip = b.Get([]byte("l"))
 		}
-		
+
 		return nil
 	})
 
