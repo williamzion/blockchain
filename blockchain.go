46f74c7d8ba967ac72b79d33e80a6618d578b823
Willaim <1340440725fhs@gmail.com>
2019-03-08 21:58:53 +0800

    Update blockchain.go
    Update comments;
    Update AddBlock with db persistence;

--- blockchain.go
+++ blockchain.go
@@ -16,18 +16,46 @@ const (
 // It’s an ordered, back-linked list. Which means that blocks are stored in the insertion order and that each block is linked to the previous one.
 // For simplicity, the Blockchain only keeps a slice of blocks, no map implementation.
 type Blockchain struct {
-	tip []byte
-	db  *bolt.DB
+	tip []byte   // hash of last block in blockchain.
+	db  *bolt.DB // persistent db connection.
 }
 
 // AddBlock saves provided data as a block adding to the blockchain.
 func (bc *Blockchain) AddBlock(data string) {
-	prev := bc.blocks[len(bc.blocks)-1]
-	new := NewBlock(data, prev.Hash)
-	bc.blocks = append(bc.blocks, new)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// Update bockchain tip once a new block was added.
+		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
 }
 
 // NewBlockChain returns a new blockchain with genesis block.
+// A db connection included in the returned value is intended to be reused.
 func NewBlockChain() *Blockchain {
 	var tip []byte
 	db, err := bolt.Open(dbFile, 0600, nil)
