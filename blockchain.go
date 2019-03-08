55ff12b635d048d18ace5a4424a903e62031f70f
Willaim <1340440725fhs@gmail.com>
2019-03-08 22:34:27 +0800

    Add BlockchainIterator to iterate over the whole blcokchian

--- blockchain.go
+++ blockchain.go
@@ -16,8 +16,18 @@ const (
 // It’s an ordered, back-linked list. Which means that blocks are stored in the insertion order and that each block is linked to the previous one.
 // For simplicity, the Blockchain only keeps a slice of blocks, no map implementation.
 type Blockchain struct {
-	tip []byte   // hash of last block in blockchain.
-	db  *bolt.DB // persistent db connection.
+	// Hash of last block in blockchain. It is an identifier of a blockchain.
+	tip []byte
+	// persistent db connection.
+	db *bolt.DB
+}
+
+// BlockchainIterator represents an iterator over blockchain blocks.
+// It contains currentHash cursor and a db connection and returns the next
+// block from a blockchain.
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
 }
 
 // AddBlock saves provided data as a block adding to the blockchain.
@@ -52,6 +62,30 @@ func (bc *Blockchain) AddBlock(data string) {
 
 		return nil
 	})
+}
+
+// Iterator returns an iterator of a blockchain at current state starting from the tip.
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+	return bci
+}
+
+// Next represents a iterator cursor returns te next block in blockchain starting from top to bottom, from newest to oldest.
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+	return block
 }
 
 // NewBlockChain returns a new blockchain with genesis block.
