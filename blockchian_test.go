380caa8679fad293d1d6bd2356f31c4145672694
Willaim <1340440725fhs@gmail.com>
2019-03-09 12:37:04 +0800

    Update TestNewBlockchain with db persistence

--- blockchian_test.go
+++ blockchian_test.go
@@ -8,19 +8,27 @@ import (
 
 func TestNewBlockchain(t *testing.T) {
 	blockchain := NewBlockChain()
+	defer blockchain.db.Close()
 
 	blockchain.AddBlock("transacton 1")
 	blockchain.AddBlock("transacton 2")
 	blockchain.AddBlock("transacton 3")
 
-	for _, block := range blockchain.blocks {
-		fmt.Printf("Prev Hash: %x\n", block.PrevBlockHash)
-		fmt.Println("Data:", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
+	it := blockchain.Iterator()
+
+	for {
+		block := it.Next()
+
+		fmt.Printf("Previous hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 
-		pow := NewProofOfWork(block)
-		fmt.Printf("pow:%s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+		// When there is no more block.
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
 	}
 }
