6f5b2b6441149f2bf6784702ad4001004feca692
Willaim <1340440725fhs@gmail.com>
2019-03-10 15:30:40 +0800

    Unspent transaction outputs: NewUTXOTransaction

--- transaction.go
+++ transaction.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+)
 
 // subsidy is the amount of reward.
 const subsidy = 10
@@ -61,4 +65,53 @@ func NewCoinbaseTX(to, data string) *Transaction {
 
 	tx.SetID()
 	return &tx
+}
+
+// NewUTXOTransaction creates a new transaction.
+func NewUTXOTransaction(from, to string, amount int, bc *Blockchain) *Transaction {
+	var (
+		inputs  []TXInput
+		outputs []TXOutput
+	)
+
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+	if acc < amount {
+		log.Panic("error: not enough funds")
+	}
+
+	// Build a list of inputs.
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+
+		for _, out := range outs {
+			input := TXInput{
+				Txid:      txID,
+				Vout:      out,
+				ScriptSig: from,
+			}
+			inputs = append(inputs, input)
+		}
+	}
+
+	// Build a list of outputs.
+	// outputs that’s locked with the receiver address. This is the actual transferring of coins to other address.
+	outputs = append(outputs, TXOutput{
+		Value:        amount,
+		ScriptPubKey: to,
+	})
+	if acc > amount {
+		// outputs that’s locked with the sender address. This is a change.
+		outputs = append(outputs, TXOutput{
+			Value:        acc - amount, // a change
+			ScriptPubKey: from,
+		})
+	}
+
+	tx := Transaction{
+		ID:   nil,
+		Vin:  inputs,
+		Vout: outputs,
+	}
+	tx.SetID()
+	return tx
 }
