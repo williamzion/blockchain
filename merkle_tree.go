0c960951e421e4e5033e0694d53cab33ac71eebc
Willaim <1340440725fhs@gmail.com>
2019-03-12 22:08:44 +0800

    Add NewMerkleTree

--- merkle_tree.go
+++ merkle_tree.go
@@ -32,3 +32,33 @@ func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 
 	return &mNode
 }
+
+// NewMerkleTree creates a new Merkle tree from a sequence of data.
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+
+	if len(data)%2 != 0 {
+		// Double odd node to nodes.
+		data = append(data, data[len(data)-1])
+	}
+
+	for _, datum := range data {
+		node := NewMerkleNode(nil, nil, datum)
+		nodes = append(nodes, *node)
+	}
+
+	for i := 0; i < len(data)/2; i++ {
+		var newLevel []MerkleNode
+
+		for j := 0; j < len(nodes); j += 2 {
+			node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+			newLevel = append(newLevel, *node)
+		}
+
+		nodes = newLevel
+	}
+
+	mTree := MerkleTree{&nodes[0]}
+
+	return &mTree
+}
