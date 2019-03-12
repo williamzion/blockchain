118cf40ac146df0cb71fc2dffcfad2e8695dd7d7
Willaim <1340440725fhs@gmail.com>
2019-03-12 21:30:38 +0800

    Add NewMerkleNode

--- merkle_tree.go
+++ merkle_tree.go
@@ -14,4 +14,21 @@ type MerkleNode struct {
 	Data  []byte
 }
 
+// NewMerkleNode creates a new Merkle tree node.
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	mNode := MerkleNode{}
 
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		mNode.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		mNode.Data = hash[:]
+	}
+
+	mNode.Left = left
+	mNode.Right = right
+
+	return &mNode
+}
