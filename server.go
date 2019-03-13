78b027dd5cc7fdfdb888b50d973d1d5618d1a474
Willaim <1340440725fhs@gmail.com>
2019-03-13 16:42:50 +0800

    Add handleTx

--- server.go
+++ server.go
@@ -362,3 +362,70 @@ func handleBlock(request []byte, bc *Blockchain) {
 		UTXOSet.Reindex()
 	}
 }
+
+func handleTx(request []byte, bc *Blockchain) {
+	var (
+		buff    bytes.Buffer
+		payload tx
+	)
+
+	buff.Write(request[commandLength:])
+	dec := gob.NewDecoder(&buff)
+	err := dec.Decode(&payload)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	txData := payload.Transaction
+	tx := DeserializeTransaction(txData)
+	mempool[hex.EncodeToString(tx.ID)] = tx
+
+	if nodeAddr == knownNodes[0] {
+		for _, node := range knownNodes {
+			if node != nodeAddr && node != payload.AddrFrom {
+				sendInv(node, "tx", [][]byte{tx.ID})
+			}
+		}
+	} else {
+		if len(mempool) >= 2 && len(miningAddr) > 0 {
+		MineTransactions:
+			var txs []*Transaction
+
+			for id := range mempool {
+				tx := mempool[id]
+				if bc.VerifyTransaction(&tx) {
+					txs = append(txs, &tx)
+				}
+			}
+
+			if len(txs) == 0 {
+				fmt.Println("All transactions are invalid! Waiting for new ones...")
+				return
+			}
+
+			cbTx := NewCoinbaseTX(miningAddr, "")
+			txs = append(txs, cbTx)
+
+			newBlock := bc.MineBlock(txs)
+			UTXOSet := UTXOSet{bc}
+			UTXOSet.Reindex()
+
+			fmt.Println("New block is mined!")
+
+			for _, tx := range txs {
+				txID := hex.EncodeToString(tx.ID)
+				delete(mempool, txID)
+			}
+
+			for _, node := range knownNodes {
+				if node != nodeAddr {
+					sendInv(node, "block", [][]byte{newBlock.Hash})
+				}
+			}
+
+			if len(mempool) > 0 {
+				goto MineTransactions
+			}
+		}
+	}
+}
--- transaction.go
+++ transaction.go
@@ -114,6 +114,19 @@ func (tx Transaction) Serialize() []byte {
 	return encoded.Bytes()
 }
 
+// DeserializeTransaction deserializes a transaction
+func DeserializeTransaction(data []byte) Transaction {
+	var transaction Transaction
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&transaction)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return transaction
+}
+
 // Verify verifies signatures of Transaction inputs
 func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool {
 	if tx.IsCoinbase() {
